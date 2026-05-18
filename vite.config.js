13bb27c25465558d284831bc8fb433900838e588
Henry David Lie <henrydavidlie@gmail.com>
2026-05-18 11:24:59 +0700

    Fix base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/p16m-simulator/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
